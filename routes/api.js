f65671a2856720ac5b098cc9e1a99ab568795176
luiggy macias <ferrinluiggy@gmail.com>
2021-12-29 15:00:16 -0500

    adding models

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,37 @@
-'use strict';
-
+"use strict";
+const mongoose = require("mongoose");
+const { IssueModel, ProjectModel } = require("../models");
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      const { issue_title, issue_text, created_by, assigned_to, status_text } =
+        req.body;
+      if (!issue_title || !issue_text || !created_by)
+        return res.status(400).send({ error: "required field(s) missing" });
+      const newIssue = new IssueModel({
+        issue_title: issue_title || "",
+        issue_text: issue_text || "",
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: created_by || "",
+        assigned_to: assigned_to || "",
+        open: true,
+        status_text: status_text || "",
+      });
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
